57d82b2610e19ef1ced3780fb82f303b8692c371
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-03-08 17:30:25 +0900

    [add] robots.tsを新規作成

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://hika-design.com/sitemap.xml",
+  };
+}
