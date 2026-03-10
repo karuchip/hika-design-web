38d557cb2f02a47d2a09b50cb1af36bf122ebd04
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-03-10 15:57:18 +0900

    [modified] ブログ一覧にて、sortで日付を昇順表示に変更

--- app/blog/BlogClient.tsx
+++ app/blog/BlogClient.tsx
@@ -37,38 +37,34 @@ const BlogClient = () => {
 
         {/* grid-cols-1 (スマホ) / md:grid-cols-2 (タブレット) / lg:grid-cols-3 (PC) */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
-          {posts.map(post => (
+          {posts
+            .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+            .map(post => (
 
-            <Link key={post.id} href={`/blog/${post.id}`}>
+              <Link key={post.id} href={`/blog/${post.id}`}>
 
-              <div className="w-[300px]">
-                <p className="w-[300px] text-[#586869] text-right my-[5px]">{new Date(post.created_at).toLocaleDateString()}</p>
-                <div className="relative">
-                  <p className={`w-fit px-[40px] py-[3px] absolute z-[1] text-[#ffffff] rounded-lg text-[18px] ${CategoryColors[post.category] || CategoryColors.default}`}>
-                    {post.category}
-                  </p>
+                <div className="w-[300px]">
+                  <p className="w-[300px] text-[#586869] text-right my-[5px]">{new Date(post.created_at).toLocaleDateString()}</p>
+                  <div className="relative">
+                    <p className={`w-fit px-[40px] py-[3px] absolute z-[1] text-[#ffffff] rounded-lg text-[18px] ${CategoryColors[post.category] || CategoryColors.default}`}>
+                      {post.category}
+                    </p>
 
-                  <div className="relative w-[300px] z-[0] h-[180px] aspect-video overflow-hidden rounded-lg">
-                    <Image
-                      src={post.topImage}
-                      alt="ブログトップ画像"
-                      width={300}
-                      height={180}
-                      className="object-cover object-center"
-                      />
+                    <div className="relative w-[300px] z-[0] h-[180px] aspect-video overflow-hidden rounded-lg">
+                      <Image
+                        src={post.topImage}
+                        alt="ブログトップ画像"
+                        width={300}
+                        height={180}
+                        className="object-cover object-center"
+                        />
+                    </div>
                   </div>
+
+                  <p className="my-[10px] w-[300px] text-[#586869] text-[18px] font-bold">{post.title}</p>
                 </div>
 
-                <p className="my-[10px] w-[300px] text-[#586869] text-[18px] font-bold">{post.title}</p>
-
-                {/* {post.blocks.map((block:BlockType) => (
-                  <div key={block.id}>
-                    <p>{JSON.stringify(block)}</p>
-                  </div>
-                ))} */}
-              </div>
-
-            </Link>
+              </Link>
 
           ))}
         </div>
