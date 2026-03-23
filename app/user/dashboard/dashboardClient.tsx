4baadc83abca9ddc7395a6c92a480ccaeae0ad3b
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-03-23 13:37:39 +0900

    [add] ログアウト機能の追加

--- app/user/dashboard/dashboardClient.tsx
+++ app/user/dashboard/dashboardClient.tsx
@@ -5,12 +5,16 @@ import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import { User } from "@supabase/supabase-js"
 import { supabase } from "@/src/lib/supabase"
+import LogoutButton from "@/app/components/user/logoutbutton"
+import { UsePost } from "@/src/hooks/usePost"
+
 
 const DashboardClient = () => {
   const router = useRouter()
   const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
 
+  // ログインチェック
   useEffect(() => {
     const getUser = async () => {
       const {
@@ -25,7 +29,6 @@ const DashboardClient = () => {
       setUser(user)
       setLoading(false)
     }
-
     getUser()
   }, [router])
 
@@ -33,7 +36,12 @@ const DashboardClient = () => {
 
   return (
     <>
-      <h1 className="pt-30">管理者画面</h1>
+      <h1 className="pt-30 text-[30px]">管理者画面</h1>
+
+      <p className="pt-10 text-[24px]">・ユーザー</p>
+      <LogoutButton/>
+
+      <p className="pt-10 text-[24px]">・ブログ</p>
       <Link href="/blog/create">ブログ作成</Link>
     </>
   )
