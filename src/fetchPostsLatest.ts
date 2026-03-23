e9d27a8e08be179c065cb34b8dd06bc051a5b759
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-03-23 20:11:34 +0900

    [modified] DBからの取得項目を追加

--- src/fetchPostsLatest.ts
+++ src/fetchPostsLatest.ts
@@ -7,7 +7,7 @@ export const fetchPostLatest = async (): Promise<{data: PostListType[]|null; err
   const {data, error, status}
     = await supabase
       .from('posts')
-      .select('id, created_at, title, topImage, category')
+      .select('id, created_at, title, topImage, category, published')
       .eq('published', true)
       .order('created_at', {ascending:false})
       .limit(4)
