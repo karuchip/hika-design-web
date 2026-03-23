9cb31b86bbe9a784d5f6eef1731518b9c0731723
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-03-23 21:14:59 +0900

    [modified] sitemapのURLを修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,7 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
     {
-      url: "https://hika-design.com/blog",
+      url: "https://hika-design.com/blog/show",
       lastModified: new Date(),
     },
   ];
