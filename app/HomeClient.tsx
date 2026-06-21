5218a42947b6aa9203d834d836e976ed8af7155d
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-06-21 13:09:25 +0900

    style: 文言修正

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -28,8 +28,8 @@ const HomeClient = () => {
             <FadeIn>
               <div className="md:absolute md:z-[1] md:left-0 md:bottom-[-400] flex items-center h-[300px] md:max-w-[680px] md:h-[330px] bg-[#ffffff] text-indigo-500 items-center justify-center shadow-xl">
                 <div>
-                  <h1 className="w-fit text-[22px] font-bold md:text-[32px] mb-5 px-15">UI/UX未経験から<br/>フロントエンド・UIデザインで<br/>仕事を得るまで</h1>
-                  <p className="px-15 md:mx-auto text-[16px] md:text-[20px]">2024年9月から趣味でWebアプリ開発を続けてきた私が、UIデザインとフロントエンドの理解を深めていく過程を記録しています</p>
+                  <h1 className="w-fit text-[22px] font-bold md:text-[32px] mb-5 px-15">未経験から<br/>フロントエンド・UIデザインで<br/>仕事を得るまで</h1>
+                  <p className="px-15 md:mx-auto text-[16px] md:text-[20px]">2024年9月から趣味でWebアプリ開発を続けてきた私が、フロントエンドとUIデザインの理解を深めていく過程を記録しています</p>
                 </div>
               </div>
             </FadeIn>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import HomeClient from "./HomeClient";
 
 export const metadata: Metadata = {
-  title: "UI/UX未経験からフロントエンド・UIデザインで仕事を得るまで",
+  title: "未経験からフロントエンド・UIデザインで仕事を得るまで",
 };
 
 export default function Page() {
